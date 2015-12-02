d013c4e03607982a908c6a515bcbab0d0ee07669
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-12-01 21:11:35 -0800

    Update migrations for production

--- db/migrate/20151202044951_add_image_to_listings.rb
+++
@@ -1,9 +0,0 @@
-class AddImageToListings < ActiveRecord::Migration
-  def self.up
-    add_attachment :listings, :image
-  end
-
-  def self.down
-    remove_attachment :listings, :image
-  end
-end
