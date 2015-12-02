c1caaae3c04740ae053434f0e5740c472eee1fea
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-12-02 13:04:28 -0800

    Add user and listing validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,8 @@ class Listing < ActiveRecord::Base
 										default_url: "noImage.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :image, attachment_presence: true
+
 end
