9912f1b798a4a46589e125006890591ee759a04d
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-12-01 19:24:57 -0800

    Add paperclip gem and update page layouts

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,7 @@
 class Listing < ActiveRecord::Base
+
+	has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, 
+										default_url: "noImage.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
